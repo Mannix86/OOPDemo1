923902ce38caa60f4a1a0070efc513a8fb40b78c
Mannix86 <mannix_e30@hotmail.com>
2018-08-24 09:21:17 +0100

    Importing Project

---
+++ oopSkillsDemo1/PetrolPump.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace oopSkillsDemo1
+{
+    class PetrolPump
+    {   
+        //declaring variables
+        protected double litrePrice;
+        protected double litreAmount;
+        protected double minimumQuantity;
+        protected string fuelType;
+
+        //default constructor, setting the variable to zero
+        /// <summary>
+        /// Default constructor, resetting the variable to no value or zero
+        /// </summary>
+        public PetrolPump()
+        {
+            litrePrice = 0;
+            litreAmount = 0;
+            minimumQuantity = 0;
+            fuelType = "";
+        }
+
+        //overloaded constructor, taking in arguments and using these to set the values in the set methods
+        /// <summary>
+        /// Overloaded constructor, passing in arguments with values
+        /// </summary>
+        /// <param name="myLitre"></param>
+        /// <param name="myAmount"></param>
+        /// <param name="minOrder"></param>
+        /// <param name="whatFuel"></param>
+        public PetrolPump(double myLitre, double myAmount, double minOrder, string whatFuel)
+        {
+            addPetrol(myAmount);
+            this.fuelType = whatFuel;
+            this.setMinQuantity(minOrder);
+            this.setPrice(myLitre);
+        }
+        //method to set the litre price
+        /// <summary>
+        /// Sets the price per litre
+        /// </summary>
+        /// <param name="myPrice"></param>
+        public void setPrice(double myPrice)
+        {
+            this.litrePrice = myPrice;
+        }
+        //method to get the litre price
+        /// <summary>
+        /// Retrieves the price per litre
+        /// </summary>
+        /// <returns></returns>
+        public double getPrice()
+        {
+            return this.litrePrice;
+        }
+        //method to get the amount in stock
+        /// <summary>
+        /// Retrieves the amount of litres in stock
+        /// </summary>
+        /// <returns></returns>
+        public double getAmount()
+        {
+            return this.litreAmount;
+        }
+        //method to set the minimum order quantity
+        /// <summary>
+        /// Sets the minimum amount of litres that can be ordered
+        /// </summary>
+        /// <param name="myMin"></param>
+        public void setMinQuantity(double myMin)
+        {
+            this.minimumQuantity = myMin;
+        }
+        //method to get the minimum order quantity
+        /// <summary>
+        /// Retrieves the minimum order quantity
+        /// </summary>
+        /// <returns></returns>
+        public double getMin()
+        {
+            return this.minimumQuantity;
+        }
+        //mthod to get the fuel type
+        /// <summary>
+        /// Retrieves the type of fuel
+        /// </summary>
+        /// <returns></returns>
+        public string getType()
+        {
+            return this.fuelType;
+        }
+        //method to add litres into the current stock level
+        /// <summary>
+        /// Adds the quantity of litres inputted to the current stock
+        /// </summary>
+        /// <param name="petrolIn"></param>
+        public void addPetrol(double petrolIn)
+        {
+            this.litreAmount = this.litreAmount + petrolIn;
+           // Console.WriteLine("Litres currently in tank : " + getAmount());
+        }
+        //method to fill the customers tank from the pump
+        /// <summary>
+        /// Takes in the value of required fuel from the user
+        /// </summary>
+        /// <param name="fuelRequired"></param>
+        /// <returns></returns>
+        public double fillTankFromPump(double fuelRequired)
+        {
+            //telling the customer they must order over the minimum order quantity
+            if (fuelRequired <  minimumQuantity)
+            {
+                Console.WriteLine("You must order " + minimumQuantity + " litres or more" );
+            }
+            //telling the customer that there is an issue when there is not adequate stock to fulfill order
+            else if (fuelRequired > litreAmount)
+            {
+                Console.WriteLine("We can not process your order, please contact a member of staff");
+                Console.ReadKey();
+                Console.Clear();
+            }
+            //processing the order, displaying the cost to the customer and updating the stock level
+            else
+            {
+                Console.WriteLine("The total cost : " + fuelRequired * getPrice());
+                this.litreAmount = this.litreAmount - fuelRequired;
+                Console.ReadKey();
+                Console.Clear();
+                return fuelRequired * getPrice();
+               
+            }
+
+            return fuelRequired * getPrice();
+        }
+
+    }
+}
---
+++ oopSkillsDemo1/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace oopSkillsDemo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   //declare variable
+            double fuel;
+            int decisionOne = 1;
+
+            //create a new instance of the PetrolPump class, passing in arguments
+            PetrolPump myPump = new PetrolPump(.75, 20000.00, 2.0, "Unleaded 98");
+            Console.WriteLine("  _______________________________________________________________\n");
+            Console.WriteLine("\t\t\tFUEL TO YOU");
+            Console.WriteLine("\tPlease choose an option :");
+            Console.WriteLine("\t1/ Fill fuel");
+            Console.WriteLine("\t2/ Fill stock tank");
+            Console.WriteLine("\t3/ Display stock level");
+            Console.WriteLine("  _______________________________________________________________");
+            bool decisionTwo = true;
+            while (decisionTwo == true)            
+            {
+                try
+                {
+                    decisionOne = Convert.ToInt32(Console.ReadLine());
+                    if (decisionOne == 1 || decisionOne == 2 || decisionOne == 3)
+                    {
+                        decisionTwo = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please enter a valid choice");
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Please enter a valid choice");
+                }
+            }
+            Console.Clear();
+            //Allowing the user to fill a vehicle from the stock levels
+            if (decisionOne == 1)
+            {
+                //Getting the user to enter the amount of fuel required
+                Console.WriteLine("  _______________________________________________________________\n");
+                Console.WriteLine("\t\t\tFUEL TO YOU");
+                Console.WriteLine("\tPlease enter the quantity of fuel required in litres");
+                Console.WriteLine("  _______________________________________________________________");
+
+                bool decision = true;
+                while (decision == true)
+                {
+                    try
+                    {
+                        Console.Write("Litres : ");
+                        fuel = (Convert.ToDouble(Console.ReadLine()));
+                        myPump.fillTankFromPump(fuel);
+                        decision = false;
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Please enter a numerical amount");
+
+                    }
+                }
+            }
+            //Allowing the user to adjust stock levels
+            if (decisionOne == 2)
+            {
+                Console.Write("Please enter the quantity you wish to add to stock : ");
+                myPump.addPetrol(Convert.ToInt32(Console.ReadLine()));
+            }
+            //Allowing the user to show stock levels
+            if (decisionOne == 3)
+            {
+                Console.WriteLine("There are " + myPump.getAmount() + " Litres in the tank");
+            }
+            
+            //adding 1000 litres as requested and displaying the amount as per task requirements
+            myPump.addPetrol(1000);
+            Console.WriteLine("There are " + myPump.getAmount() + " Litres in the tank");
+        }
+    }
+}
